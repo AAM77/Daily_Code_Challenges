bc3446544bf77ee4c0879e73f87f91abef33a759
AAM77 <Adeel.A.Mhd@gmail.com>
2019-02-21 23:37:15 -0500

    Added count_pos_sum_neg challenge

--- codewars/8_kyu/python3/count_pos_sum_neg.py
+++ codewars/8_kyu/python3/count_pos_sum_neg.py
@@ -1,9 +1,9 @@
-# Name: [Challenge Name]
-# Difficulty: [(e.g. 8 kyu, 7 kyu, easy, medium, 6 kyu, etc.)]
+# Name: Count of positives / sum of negatives
+# Difficulty: 8 kyu
 #
 # --- sources ---
-# Website: [(e.g. CodeWars, LeetCode, HackerRank, etc.)]
-# URL: [(e.g. https://...)]
+# Website: CodeWars
+# URL: https:#www.codewars.com/kata/count-of-positives-slash-sum-of-negatives/train/javascript
 #
 #
 #
@@ -12,8 +12,15 @@
 #  Instructions  #
 #                #
 ##################
+
+# Given an array of integers.
 #
+# Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers.
 #
+# If the input array is empty or null, return an empty array.
+#
+# Example
+# For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
 
 ##############
 # PSEUDOCODE #
@@ -29,3 +36,20 @@
 #***********#
 # Attempt 1 #
 #***********#
+
+def count_positives_sum_negatives(arr):
+    new_arr = []
+
+    if len(arr) > 0:
+        pos_count = 0
+        neg_total = 0
+
+        for num in arr:
+            if num>0:
+                pos_count += 1
+            elif num<0:
+                neg_total += num
+
+        return [pos_count, neg_total]
+    else:
+        return []
