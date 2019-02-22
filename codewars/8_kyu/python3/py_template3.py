d10054faa61f65282ebc242700d0557f7e05157a
AAM77 <Adeel.A.Mhd@gmail.com>
2019-02-21 23:28:59 -0500

    Added new challenge and tempaltes

---
+++ codewars/8_kyu/python3/py_template3.py
@@ -0,0 +1,31 @@
+# Name: [Challenge Name]
+# Difficulty: [(e.g. 8 kyu, 7 kyu, easy, medium, 6 kyu, etc.)]
+#
+# --- sources ---
+# Website: [(e.g. CodeWars, LeetCode, HackerRank, etc.)]
+# URL: [(e.g. https://...)]
+#
+#
+#
+##################
+#                #
+#  Instructions  #
+#                #
+##################
+#
+#
+
+##############
+# PSEUDOCODE #
+##############
+#
+
+
+
+#############
+# SOLUTIONS #
+#############
+
+#***********#
+# Attempt 1 #
+#***********#
