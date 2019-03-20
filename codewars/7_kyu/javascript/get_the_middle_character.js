6727f49c055e8c43cc97593c589ad80aecf741f5
AAM77 <Adeel.A.Mhd@gmail.com>
2019-03-19 23:05:29 -0400

    A challenge I completed today.

---
+++ codewars/7_kyu/javascript/get_the_middle_character.js
@@ -0,0 +1,67 @@
+// Name: Get the Middle Character
+// Difficulty: 7 kyu
+//
+// --- sources ---
+// Website: CodeWars
+// URL: https://www.codewars.com/kata/get-the-middle-character/train/javascript
+//
+//
+
+////////////////////
+//                //
+//  Instructions  //
+//                //
+////////////////////
+// You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+//
+// #Examples:
+//
+// Kata.getMiddle("test") should return "es"
+//
+// Kata.getMiddle("testing") should return "t"
+//
+// Kata.getMiddle("middle") should return "dd"
+//
+// Kata.getMiddle("A") should return "A"
+// #Input
+//
+// A word (string) of length 0 < str < 1000 (In javascript you may get slightly more than 1000 in some test cases due to an error in the test cases). You do not need to test for this. This is only here to tell you that you do not need to worry about your solution timing out.
+//
+// #Output
+//
+// The middle character(s) of the word represented as a string.
+
+/////////////////
+// PSEUDOCODE //
+/////////////////
+
+// Objectives:
+  // - if string length odd: return middle char
+  // - if string length even: return middle 2 chars
+
+// PSEUDOCODE
+// check the length of the string
+// if odd length: get the element in
+// the Math.floor(length/2) index
+// if even length: get the elements in the Math.floor(length/2) - 1 and Math.floor(length/2) positions
+
+
+///////////////
+// SOLUTIONS //
+///////////////
+
+//***********//
+// Attempt 1 //
+//***********//
+
+
+function getMiddle(s)
+{
+  const halfLength = Math.floor(s.length/2)
+
+  if (s.length%2 === 0) {
+    return `${s[halfLength-1]}${s[halfLength]}`
+  } else {
+    return `${s[halfLength]}`
+  }
+}
