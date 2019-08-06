e1a313f8acba89db1836e12d5f67edcfc810bfba
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-08-06 16:53:11 -0400

    Added a link, reorganized, and added a refactored version.

--- codewars/6_kyu/ruby/array2_elements_in_array1.rb
+++ codewars/6_kyu/ruby/array2_elements_in_array1.rb
@@ -1,31 +1,36 @@
-def contains_all?(haystack, needle)
-	# PSEUDOCODE
-    # Objective
-      # check if the 'haystack' array contains the 'needle'
-      # must be strict
-        # (must match type)
-        # (must account for repeats)
+# https://www.codewars.com/kata/does-an-array-contain-all-elements-from-another-array/train/ruby
 
-  # STEPS
-    # create a needle_hash
-    # iterate through the needle
-      # if in needle_hash
+# PSEUDOCODE
+  # Objective
+    # check if the 'haystack' array contains the 'needle'
+    # must be strict
+      # (must match type)
+      # (must account for repeats)
+
+# STEPS
+  # create a needle_hash
+  # iterate through the needle
+    # if in needle_hash
+      # increment value by 1
+    # else
+      # create a key for each with a value (counter) of 1
+
+  # create a haystack_nums hash
+  # iterate through the haystack
+    # if in needle_targets hash
+      # if in haystack_nums has:
         # increment value by 1
       # else
-        # create a key for each with a value (counter) of 1
+        # add as key to haystack_nums w/ a value of 1
 
-    # create a haystack_nums hash
-    # iterate through the haystack
-      # if in needle_targets hash
-        # if in haystack_nums has:
-          # increment value by 1
-        # else
-          # add as key to haystack_nums w/ a value of 1
+  # if needle_targets == haystack_nums
+      # return true
+  # else
+      # return false
 
-    # if needle_targets == haystack_nums
-        # return true
-    # else
-        # return false
+
+# ATTEMPT 1
+def contains_all?(haystack, needle)
 
   needle_hash = {}
   haystack_hash = {}
@@ -53,4 +58,28 @@ def contains_all?(haystack, needle)
   else
     false
   end
+end
+
+# ATTEMPT 1 - REFACTORED
+
+def contains_all?(haystack, needle)
+
+  needle_hash = {}
+  haystack_hash = {}
+
+  needle.each { |num| needle_hash[num] ? needle_hash[num] += 1 : needle_hash[num] = 1 }
+
+  haystack.each do |num|
+    if needle_hash[num]
+
+      if !haystack_hash[num]
+        haystack_hash[num] = 1
+      elsif (haystack_hash[num] < needle_hash[num])
+        haystack_hash[num] += 1
+      end
+
+    end
+  end
+
+  needle_hash == haystack_hash ? true : false
 end
