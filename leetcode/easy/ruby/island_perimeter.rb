99b6bcc18b0c027d9bb06f29e8bb5281f43cd0da
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-09-05 17:42:39 -0400

    fixed indentations.

--- leetcode/easy/ruby/island_perimeter.rb
+++ leetcode/easy/ruby/island_perimeter.rb
@@ -44,46 +44,46 @@
 
 def island_perimeter(grid)
 
-    perimeter = 0
+  perimeter = 0
 
-    grid.each.with_index do |a, i|
-        a.each.with_index do |n, j|
-            if n == 1
-                surrounding_ones = 0
+  grid.each.with_index do |a, i|
+    a.each.with_index do |n, j|
+      if n == 1
+        surrounding_ones = 0
 
-                if a[j-1] == 1 && j != 0
-                    surrounding_ones += 1
-                end
-
-                if a[j+1] == 1
-                    surrounding_ones += 1
-                end
-
-                if (i != 0) && grid[i-1][j] == 1
-                    surrounding_ones += 1
-                end
-
-                if (i != grid.length - 1) && (grid[i+1][j] == 1)
-                    surrounding_ones += 1
-                end
-
-
-                if surrounding_ones == 0
-                    perimeter += 4
-                elsif surrounding_ones == 1
-                    perimeter += 3
-                elsif surrounding_ones == 2
-                    perimeter += 2
-                elsif surrounding_ones == 3
-                    perimeter += 1
-                elsif surrounding_ones == 4
-                    perimeter += 0
-                end
-            end
+        if a[j-1] == 1 && j != 0
+            surrounding_ones += 1
         end
-    end
 
-    perimeter
+        if a[j+1] == 1
+            surrounding_ones += 1
+        end
+
+        if (i != 0) && grid[i-1][j] == 1
+            surrounding_ones += 1
+        end
+
+        if (i != grid.length - 1) && (grid[i+1][j] == 1)
+            surrounding_ones += 1
+        end
+
+
+        if surrounding_ones == 0
+            perimeter += 4
+        elsif surrounding_ones == 1
+            perimeter += 3
+        elsif surrounding_ones == 2
+            perimeter += 2
+        elsif surrounding_ones == 3
+            perimeter += 1
+        elsif surrounding_ones == 4
+            perimeter += 0
+        end
+      end
+    end
+  end
+
+  perimeter
 
 end
 
@@ -91,34 +91,34 @@ end
 
 def island_perimeter(grid)
 
-    perimeter = 0
+  perimeter = 0
 
-    grid.each.with_index do |a, i|
-      a.each.with_index do |n, j|
-        if n == 1
-          surrounding_ones = 0
+  grid.each.with_index do |a, i|
+    a.each.with_index do |n, j|
+      if n == 1
+        surrounding_ones = 0
 
-          surrounding_ones += 1 if a[j-1] == 1 && j != 0
-          surrounding_ones += 1 if a[j+1] == 1
-          surrounding_ones += 1 if (i != 0) && grid[i-1][j] == 1
-          surrounding_ones += 1 if (i != grid.length - 1) && (grid[i+1][j] == 1)
+        surrounding_ones += 1 if a[j-1] == 1 && j != 0
+        surrounding_ones += 1 if a[j+1] == 1
+        surrounding_ones += 1 if (i != 0) && grid[i-1][j] == 1
+        surrounding_ones += 1 if (i != grid.length - 1) && (grid[i+1][j] == 1)
 
 
-          case surrounding_ones
-          when 0
-            perimeter += 4
-          when 1
-            perimeter += 3
-          when 2
-            perimeter += 2
-          when 3
-            perimeter += 1
-          when 4
-            perimeter += 0
-          end
+        case surrounding_ones
+        when 0
+          perimeter += 4
+        when 1
+          perimeter += 3
+        when 2
+          perimeter += 2
+        when 3
+          perimeter += 1
+        when 4
+          perimeter += 0
         end
       end
     end
+  end
 
-    perimeter
+  perimeter
 end
