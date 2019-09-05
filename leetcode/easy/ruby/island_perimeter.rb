f2ca46ac6286624da2ebc7e14ed4d28bf2b79865
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-09-05 17:40:40 -0400

    added refactored solution.

--- leetcode/easy/ruby/island_perimeter.rb
+++ leetcode/easy/ruby/island_perimeter.rb
@@ -1,4 +1,7 @@
 
+# https://leetcode.com/problems/island-perimeter/
+
+
 
 # @param {Integer[][]} grid
 # @return {Integer}
@@ -82,4 +85,40 @@ def island_perimeter(grid)
 
     perimeter
 
+end
+
+# ATTEMPT 1 refactored
+
+def island_perimeter(grid)
+
+    perimeter = 0
+
+    grid.each.with_index do |a, i|
+      a.each.with_index do |n, j|
+        if n == 1
+          surrounding_ones = 0
+
+          surrounding_ones += 1 if a[j-1] == 1 && j != 0
+          surrounding_ones += 1 if a[j+1] == 1
+          surrounding_ones += 1 if (i != 0) && grid[i-1][j] == 1
+          surrounding_ones += 1 if (i != grid.length - 1) && (grid[i+1][j] == 1)
+
+
+          case surrounding_ones
+          when 0
+            perimeter += 4
+          when 1
+            perimeter += 3
+          when 2
+            perimeter += 2
+          when 3
+            perimeter += 1
+          when 4
+            perimeter += 0
+          end
+        end
+      end
+    end
+
+    perimeter
 end
